30f505d316d93c09e60c243f54f86f6234bbdc44
FThappy <thangchu766@gmail.com>
2024-06-04 23:36:16 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-// import authRoute from "./routes/auth.route.js";
+import authRoute from "./routes/auth.route.js";
 // import postRoute from "./routes/post.route.js";
 // import userRoute from "./routes/user.route.js";
 // import reportRoute from "./routes/report.route.js";
@@ -80,7 +80,7 @@ const io = new Server(server, {
 // io.on("connection", onConnection);
 app.use(express.json());
 app.use(cookieParser());
-// app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute);
 // app.use("/api/post", postRoute);
 // app.use("/api/user", userRoute);
 // app.use("/api/report", reportRoute);
--- routes/test.route.js
+++ routes/test.route.js
@@ -3,6 +3,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("", testControler);
+router.get("", testControler);
 
 export default router;
