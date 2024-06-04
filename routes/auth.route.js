111a1551a7e1dafeab579facde99abb59401e5f2
FThappy <thangchu766@gmail.com>
2024-06-05 00:29:02 +0700

    a

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,19 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
+
+const storage = multer.memoryStorage();
+const upload_image = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      req.multerError = true;
+      cb(null, false, req.multerError);
+    }
+  },
+});
 router.post("/register", register);
 
 router.post("/login", login);
