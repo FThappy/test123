804ba1ad715c7db1c9c0dc9b3771fc89289ee829
FThappy <thangchu766@gmail.com>
2024-06-04 22:59:51 +0700

    new

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js";
-import userRoute from "./routes/user.route.js";
-import reportRoute from "./routes/report.route.js";
-import eventRoute from "./routes/event.route.js";
-import projectRoute from "./routes/project.route.js";
+// import authRoute from "./routes/auth.route.js";
+// import postRoute from "./routes/post.route.js";
+// import userRoute from "./routes/user.route.js";
+// import reportRoute from "./routes/report.route.js";
+// import eventRoute from "./routes/event.route.js";
+// import projectRoute from "./routes/project.route.js";
 import cookieParser from "cookie-parser";
 import { Server } from "socket.io";
 import http from "http";
 import jwt from "jsonwebtoken";
-import {
-  changeComment,
-  deleteComment,
-  joniRoom,
-  leaveRoom,
-  loadMoreComment,
-  sendComment,
-} from "./utils/socket.js";
+// import {
+//   changeComment,
+//   deleteComment,
+//   joniRoom,
+//   leaveRoom,
+//   loadMoreComment,
+//   sendComment,
+// } from "./utils/socket.js";
 const app = express();
 dotenv.config();
 
@@ -45,14 +45,14 @@ const io = new Server(server, {
     credentials: true,
   },
 });
-const onConnection = (socket) => {
-  joniRoom(io, socket);
-  sendComment(io, socket);
-  loadMoreComment(io, socket);
-  changeComment(io, socket);
-  deleteComment(io, socket);
-  leaveRoom(io, socket);
-};
+// const onConnection = (socket) => {
+//   joniRoom(io, socket);
+//   sendComment(io, socket);
+//   loadMoreComment(io, socket);
+//   changeComment(io, socket);
+//   deleteComment(io, socket);
+//   leaveRoom(io, socket);
+// };
 // io.use((socket, next) => {
 //   const handshake = socket.handshake;
 
@@ -75,10 +75,10 @@ const onConnection = (socket) => {
 //     next();
 //   });
 // });
-io.on("connection", onConnection);
+// io.on("connection", onConnection);
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoute);
+// app.use("/api/auth", authRoute);
 // app.use("/api/post", postRoute);
 // app.use("/api/user", userRoute);
 // app.use("/api/report", reportRoute);
@@ -86,7 +86,9 @@ app.use("/api/auth", authRoute);
 // app.use("/api/project", projectRoute);
 
 
-
+app.get("/api", function(req, res){
+  return res.status(200).body("alo")
+})
 
 
 server.listen(5000, () => {
--- utils/multerError.js
+++
@@ -1,6 +0,0 @@
-export const multerError = async(req,res, next)=>{
-  if(req.multerError){
-    return res.status(400).json({ message: "Chỉ chấp nhận file ảnh", code: 1 });
-  }
-  next();
-}
